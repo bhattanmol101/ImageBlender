d9136991d62c33b1a086df796226bc6bedcf01a4
Bhat, Anmol <anmol.bhat@sap.com>
2020-05-05 21:01:20 +0530

    Final release v:1.0.0 Face Blender Backend

--- facedetection.py
+++ facedetection.py
@@ -6,24 +6,17 @@ import cv2
 import os
 
 
-def getBlendedImage():
-    directory = os.fsencode('./img')
-    imgArray = []
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        if filename.endswith('.jpeg') or filename.endswith('.jpg') or filename.endswith('.png'):
-            imgArray.append(filename)
-
+def getBlendedImage(img1, img2):
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(
         "./model/shape_predictor_68_face_landmarks.dat")
     fa = FaceAligner(predictor, desiredFaceWidth=256)
 
-    image1 = cv2.imread("./img/" + imgArray[0])
+    image1 = cv2.imdecode(img1, cv2.IMREAD_COLOR)
     image1 = imutils.resize(image1, width=1000)
     gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     rects1 = detector(gray1, 2)
-    image2 = cv2.imread("./img/" + imgArray[1])
+    image2 = cv2.imdecode(img2, cv2.IMREAD_COLOR)
     image2 = imutils.resize(image2, width=1000)
     gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
     rects2 = detector(gray2, 2)
--- server.py
+++ server.py
@@ -3,78 +3,87 @@ from flask import Flask, request, Response
 from werkzeug.utils import secure_filename
 from facedetection import getBlendedImage
 from flask.helpers import send_file
+import logging
+from datetime import datetime
+from pytz import timezone
+import shutil
+import numpy as np
+
+LOGGING_FILE = 'logs/app_server.log'
+
+logging.basicConfig(
+    filename=LOGGING_FILE,
+    level=logging.INFO)
+tz = timezone('Asia/Kolkata')
+indiatime = datetime.now(tz)
+logging.info(" " + indiatime.strftime('%Y-%m-%d %H:%M:%S') +
+             ": application has started")
 
 SAVE_FOLDER = "./img/"
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-app = Flask(__name__)
-app.config['SAVE_FOLDER'] = SAVE_FOLDER
-
-if not os.path.exists('./img'):
-    os.makedirs('./img')
+app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
 
 
 @app.route("/")
 def home():
-    return "home"
+    return app.send_static_file("index.html")
 
 
-@app.route("/file1", methods=['POST'])
-def save_file1():
-    if 'file' not in request.files:
-        return Response(
-            "NoFile",
-            status=400,
-        )
-    file = request.files['file']
-    if file.filename == '':
-        return Response(
-            "NoFileName",
-            status=400,
-        )
-    if file and allowed_file(file.filename):
-        print(file)
-        filename = secure_filename(file.filename)
-        fileExtention = filename.split('.')[1]
-        file.save(os.path.join(
-            app.config['SAVE_FOLDER'], "pic1." + fileExtention))
-        return Response(
-            "Done",
-            status=200,
-        )
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 
-@app.route("/file2", methods=['POST'])
-def save_file2():
-    if 'file' not in request.files:
-        return Response(
-            "NoFile",
-            status=400,
-        )
-    file = request.files['file']
-    if file.filename == '':
-        return Response(
-            "NoFileName",
-            status=400,
-        )
-    if file and allowed_file(file.filename):
-        print(file)
-        filename = secure_filename(file.filename)
-        fileExtention = filename.split('.')[1]
-        file.save(os.path.join(
-            app.config['SAVE_FOLDER'], "pic2." + fileExtention))
-        return Response(
-            "Done",
-            status=200,
-        )
+@app.route("/count")
+def counter():
+    f = open('counter.in', 'r')
+    count = int(f.read())
+    f.close()
+    count = count + 1
+    f = open('counter.in', "w+")
+    f.write(str(count))
+    f.close()
+
+    if os.path.exists('__pycache__'):
+        shutil.rmtree('__pycache__')
+
+    if os.path.exists('./merged'):
+        shutil.rmtree('./merged')
+    os.makedirs('./merged')
+    indiatime = datetime.now(tz)
+    logging.info(" " + indiatime.strftime('%Y-%m-%d %H:%M:%S') +
+                 ": deleted old folders and created new folders")
+    return "Done"
 
 
-@app.route("/blend", methods=['GET'])
+@app.route("/blend", methods=['POST'])
 def blending_images():
-    getBlendedImage()
+    indiatime = datetime.now(tz)
+    logging.info(
+        " " + indiatime.strftime('%Y-%m-%d %H:%M:%S') + ": image blender called")
     try:
-        return send_file('./merged/pic.png', attachment_filename='pic.png')
+        images = request.files.getlist("file")
+        indiatime = datetime.now(tz)
+        logging.info(
+            " " + indiatime.strftime('%Y-%m-%d %H:%M:%S') + ": both the images received")
+        getBlendedImage(np.fromstring(images[0].read(), np.uint8), np.fromstring(
+            images[1].read(), np.uint8))
+        indiatime = datetime.now(tz)
+        logging.info(
+            " " + indiatime.strftime('%Y-%m-%d %H:%M:%S') + ": blended image sent")
+        if os.path.exists('__pycache__'):
+            shutil.rmtree('__pycache__')
+
+        return send_file("./merged/pic.png", attachment_filename='pic.png')
     except Exception as e:
+        indiatime = datetime.now(tz)
+        logging.error(
+            " " + indiatime.strftime('%Y-%m-%d %H:%M:%S') + ": error occurred while calling blendImage with error: " + e, e)
         return str(e)
 
 
@@ -84,4 +93,4 @@ def allowed_file(filename):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(threaded=True)
